9dbe8e3d49e22a0462f7f0b535cb4fc3ee7fbf96
Elliot Brookes <elliotbrookes1@gmail.com>
2023-10-23 09:13:12 +0100

    moves the packages into the pkg dir and adds in a main file

--- hello.go
+++ hello.go
@@ -1,11 +1,5 @@
 package main
 
-import "fmt"
-
 func Hello() string {
 	return "Hello, world"
-}
-
-func main() {
-	fmt.Println(Hello())
 }
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	// insert the code to be run below, playground code should have written tests before functionality
+
+	log.Println("EXECUTION TIME: ", time.Since(t))
+}
